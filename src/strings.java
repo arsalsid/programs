6f5562cd2e05d53e5b2362f7cfbb9073a94c8897
arsalsid <arsal243@gmail.com>
2020-01-16 01:28:42 +0500

    Strings file added

---
+++ src/strings.java
@@ -0,0 +1,10 @@
+public class strings {
+    public static void main(String[] args) {
+        String s = "Arsalan";
+        String space = " ";
+
+        System.out.println("Your Name is : " +s +space);
+
+
+    }
+}
