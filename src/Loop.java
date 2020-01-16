776bc9ac85b77940dfb736f70e77844fd7dc2d23
Arsalan Siddique <ASiddique@dd.local>
2020-01-16 17:37:03 +0500

    Pallindrome

---
+++ src/Loop.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Loop {
+    public static void main(String[] args) {
+    String temp, reverse;
+
+    Scanner s = new Scanner(System.in);
+        System.out.println("Enter a Pallindrome:" );
+        temp = s.nextLine();
+
+
+
+    }
+}
