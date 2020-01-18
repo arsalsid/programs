6a63b6d4d2c7f8a8f10fd04dd89de04c26a5a095
arsalsid <arsal243@gmail.com>
2020-01-19 00:02:00 +0500

    While_loop

--- src/strings.java
+++ src/strings.java
@@ -1,7 +1,7 @@
 public class strings {
     public static void main(String[] args) {
         String s = "Arsalan";
-        String space = " ";
+        String space = " "; // concatenation
 
         System.out.println("Your Name is : " +s +space);
 
---
+++ src/while_Loop.java
@@ -0,0 +1,14 @@
+
+public class while_Loop {
+    public static void main(String[] args) {
+        int num = 23;           //While loop practice//
+
+        while (num < 45) {
+            System.out.println("Okay" +" "+ num);
+
+             num = num +3;
+        }
+    }
+
+
+}
