078607aec67d41e15ab56a52f75377668200db16
arsalsid <arsal243@gmail.com>
2020-01-19 00:09:30 +0500

    String_concatenation

--- src/strings.java
+++ src/strings.java
@@ -1,10 +1,12 @@
 public class strings {
     public static void main(String[] args) {
         String s = "Arsalan";
-        String space = " "; // concatenation
+        String d =  "Software Quality Assurance Engineer";
+        String space = " ";         // concatenation
+
 
         System.out.println("Your Name is : " +s +space);
-
+        System.out.println("Your Designation as : "  +" " +d );
 
     }
 }
