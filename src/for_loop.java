9482cbd6284edfce472662325698108a714cf2ef
arsalsid <arsal243@gmail.com>
2020-01-21 01:50:05 +0500

    Multiplication tables from user input in loop

--- src/for_loop.java
+++ src/for_loop.java
@@ -1,11 +1,26 @@
 import java.util.Scanner;
 public class for_loop {
     public static void main(String[] args) {
-        String str;
-            Scanner s = new scanner (System.in);
-             for (int i = 0; i < 5; i++) {
-            System.out.println("For loop");
-            str = s.nextLine();
+
+     /*   int a = 2;  // Table of multiplication without input from user//
+
+
+        for (int i = 1; i <= 10; i++) {
+           System.out.printf("%d * %d = %d \n", a, i, a * i);
+        } */
+
+     
+     // Multiplication Table by using input by user //
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter an Input:" +" ");
+        int num = s.nextInt();
+
+        for(int i = 1; i <=10; i++){
+            System.out.println(num + "*" + i + "=" + (num * i));
         }
     }
 }
+
+
+
