60263f2c570f92c966664124e23be84ab854fb93
Arsalan Siddique <ASiddique@dd.local>
2020-01-23 16:34:56 +0500

    App task

---
+++ src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) {
+        int a = 26;
+        int b = a / 2;
+
+        for(b =13; b > 2; a++);
+        {
+            if((++a < 2) && (++b > 3));
+            {
+                b++;
+            }
+        }
+            System.out.println(a  +" ");
+            System.out.println(-b +" ");
+        }
+    }
+
