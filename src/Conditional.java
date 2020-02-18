bc7335730ade86143de4794cdb08fde90c8dabf7
Arsalan Siddique <ASiddique@dd.local>
2020-02-18 17:00:35 +0500

    error fixes

--- src/Conditional.java
+++ src/Conditional.java
@@ -5,11 +5,11 @@ public class Conditional {
         int a = 8, b = 10;
 
         Scanner i = new Scanner(System.in);
-<<<<<<< HEAD
+
         System.out.println("Enter a Value:");
-=======
+
         System.out.println("Enter a Value:" +" ");
->>>>>>> da0da7a8db2bc44d70dec5f05d95030bb6dc9748
+
         str = i.nextLine();
 
         if(a > b){
