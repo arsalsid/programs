d474e8deb272379440f7b351f3ce7502139780df
Arsalan Siddique <ASiddique@dd.local>
2020-02-18 16:54:14 +0500

    Sample and return

--- src/sample.java
+++ src/sample.java
@@ -1,14 +1,23 @@
 
 public class sample {
     int a(){
-        System.out.println("Method a return");
+        System.out.println("m1 method");
 
         return 10;
     }
-}
+    float f(){
+        System.out.println("float method");
+
+        return 20.5f;
+    }
 
     public static void main(String[] args) {
         sample s = new sample ();
         int x=s.a();
+        System.out.println("Return value of a()= " +x);
+
+        float y=s.f();
+        System.out.println("Return value of b()= " +y);
     }
 }
+
