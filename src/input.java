140730898326f7f640ff8f927ae1757cbfa25f97
arsalsid <arsal243@gmail.com>
2020-02-23 17:03:05 +0500

    input file changes

--- src/input.java
+++ src/input.java
@@ -4,13 +4,26 @@ public class input {
 
     public static void main(String[] args) {
 
-        String str;
-        String blank = " ";
+//        String str;
+//        String blank = " ";
 
         Scanner s = new Scanner (System.in);
-        System.out.println("Your First Name : " );
-        str = s.nextLine();
-        System.out.println("Hello" +blank +str);
+
+//          System.out.println("Your First Name : " );
+//          System.out.println("Enter your Text here: " );
+//          System.out.println("Enter an Decimal Number : " );
+            System.out.println("Enter a Float point number : ");
+
+//        String Text = s.nextLine();
+//        int num  = s.nextInt();
+//        double num2 = s.nextDouble();
+          float num3 = s.nextFloat();
+
+////        System.out.println("Your Text Line is: " +Text);
+////        System.out.println("Hello" +blank +str);
+//          System.out.println("Your Float Integer value is: " +num2);
+            System.out.println("Your Float Integer value is: " +num3);
+
 
     }
 }
