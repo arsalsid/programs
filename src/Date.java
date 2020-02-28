ffc4ce7cf8cc2123f81f3a0fe5db37cb7f87b3cc
Arsalan Siddique <ASiddique@dd.local>
2020-02-28 20:43:04 +0500

    Date and Day

--- src/Date.java
+++ src/Date.java
@@ -1,13 +1,20 @@
 import java.time.LocalDateTime;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 
 public class Date {
 
     public static void main(String[] args) {
+
+        int Day;
         DateTimeFormatter date = DateTimeFormatter.ofPattern("dd:MM:YYYY hh:mm:ss");
         LocalDateTime now =  LocalDateTime.now();
         System.out.println(date.format(now));
+
+        int dayOfWeek = Calendar.DAY_OF_WEEK;
+        System.out.println(Calendar.DAY_OF_WEEK);
 
     }
 }
--- src/while_Loop.java
+++ src/while_Loop.java
@@ -2,10 +2,10 @@
 public class while_Loop {
     public static void main(String[] args) {
         int num = 23;           //While loop practice//
+        double num2 = 2380.00;
 
-        while (num < 45) {
+      while (num < 45) {
             System.out.println("Okay" + " " + num);
-
             num = num + 3;
         }
     }
