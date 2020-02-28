c6713e00f18a80de12c2eb34f9b80ed86ee421fe
Arsalan Siddique <ASiddique@dd.local>
2020-02-28 20:50:12 +0500

    Date and Day

--- src/Date.java
+++ src/Date.java
@@ -8,13 +8,12 @@ public class Date {
 
     public static void main(String[] args) {
 
-        int Day;
         DateTimeFormatter date = DateTimeFormatter.ofPattern("dd:MM:YYYY hh:mm:ss");
         LocalDateTime now =  LocalDateTime.now();
         System.out.println(date.format(now));
 
-        int dayOfWeek = Calendar.DAY_OF_WEEK;
-        System.out.println(Calendar.DAY_OF_WEEK);
+        int Weekofmonth = Calendar.DAY_OF_WEEK_IN_MONTH;
+        System.out.println(Calendar.DAY_OF_WEEK_IN_MONTH);
 
     }
 }
