ded35c0c0c06a23ad2d97760cd6701e642c9c7da
arsalsid <arsal243@gmail.com>
2020-02-29 02:41:59 +0500

    Date and Calender

--- src/Date.java
+++ src/Date.java
@@ -1,6 +1,4 @@
 import java.time.LocalDateTime;
-import java.time.DayOfWeek;
-import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 
@@ -8,12 +6,23 @@ public class Date {
 
     public static void main(String[] args) {
 
+
         DateTimeFormatter date = DateTimeFormatter.ofPattern("dd:MM:YYYY hh:mm:ss");
         LocalDateTime now =  LocalDateTime.now();
         System.out.println(date.format(now));
 
-        int Weekofmonth = Calendar.DAY_OF_WEEK_IN_MONTH;
-        System.out.println(Calendar.DAY_OF_WEEK_IN_MONTH);
 
+        Calendar cal = Calendar.getInstance();
+
+        System.out.println(cal.getTime().toString());
+        System.out.println("Year = " + cal.get(Calendar.YEAR));
+        System.out.println("Month = " + cal.get(Calendar.MONTH)+1);
+        System.out.println("Week of Month = " + cal.get(Calendar.WEEK_OF_MONTH));
+        System.out.println("Days of Month = " + cal.get(Calendar.DAY_OF_MONTH));
+        System.out.println("Weeks of Year = " + cal.get(Calendar.WEEK_OF_YEAR));
+        System.out.println("Hours = " + cal.get(Calendar.HOUR));
+        System.out.println("Hours of Day = " + cal.get(Calendar.HOUR_OF_DAY));
+
+      
     }
 }
