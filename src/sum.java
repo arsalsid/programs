fc5e6d0ce6fa96cedd781164e4f0edde6f1926fe
Arsalan Siddique <ASiddique@dd.local>
2020-02-28 15:40:27 +0500

    sum

---
+++ src/sum.java
@@ -0,0 +1,8 @@
+public class sum {
+    public static void main(String[] args) {
+        int x = 559;
+        int y = Math.round(x);              //Math.Round function//
+        int z = x / y;
+        System.out.println("The Value is " +z);
+    }
+}
