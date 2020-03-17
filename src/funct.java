fe215c4a9ca0dc75ab6fe19a99c29c17336340dd
Arsalan Siddique <ASiddique@dd.local>
2020-03-17 18:18:10 +0500

    fuct

---
+++ src/funct.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class funct {
+    public static void main(String[] args) {
+        doSomething(4);
+    }
+    static void doSomething(int x)
+    {
+        System.out.println(x * x);
+    }
+}
