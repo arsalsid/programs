0522a31fea455b674512ff556eb73ea83dc72dd4
Arsalan Siddique <ASiddique@dd.local>
2020-03-19 00:42:42 +0500

    A and B

---
+++ src/A.java
@@ -0,0 +1,27 @@
+
+public class A {
+    String str;
+    private void print()
+    {
+        System.out.println("A");
+    }
+    private void print(String str)
+    {
+        System.out.println("b");
+    }
+    private void print (int x)
+    {
+        System.out.println("c");
+    }
+//    public void printd()
+//    {
+//        this.print();
+//    }
+
+    public static void main(String[] args) {
+        A obj = new A ();
+        obj.print("arsalan");
+
+
+    }
+}
---
+++ src/B.java
@@ -0,0 +1,13 @@
+public class B {
+
+    private void print(String str){
+
+        System.out.println("d");
+    }
+
+    public static void main(String[] args) {
+        B obj = new B ();
+        obj.print("arsalan");
+    }
+}
+
